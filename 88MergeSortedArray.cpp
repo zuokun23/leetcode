8bdf477eff66fafb780fb6526769bd9b913aa820
voiletshellz <18873023176@qq.com>
2018-06-15 08:59:37 +0800

    Create 88MergeSortedArray.cpp

---
+++ 88MergeSortedArray.cpp
@@ -0,0 +1,34 @@
+//88:给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+//说明:
+//初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+//你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+//归并排序的思想
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if( n == 0 )
+            return;
+        if( n!= 0 && m == 0){
+            for(int i = 0 ; i < n ;i++){
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+        //算法开始
+        int aux[m];//辅助空间
+        for(int i = 0;i < m ; i++)
+            aux[i] = nums1[i];
+        
+        int i = 0 ,j = 0;
+        for(int k = 0 ; k < m+n ; k++){
+            if(i >= m)
+                nums1[k] = nums2[j++];
+            else if(j >= n)
+                nums1[k] = aux[i++];
+            else if(aux[i] <= nums2[j])
+                nums1[k] = aux[i++];
+            else
+                nums1[k] = nums2[j++];
+        }
+    }
+};
