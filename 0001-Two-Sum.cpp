8562a8b882ed379bbedb710c92a4df754837bde8
zuokun23 <1291136919@qq.com>
2018-06-24 15:56:09 +0800

    Update 0001-Two-Sum.cpp

--- 0001-Two-Sum.cpp
+++ 0001-Two-Sum.cpp
@@ -21,3 +21,31 @@ public:
         throw invalid_argument("the input has no solution");
     }
 };
+
+// 由于题目中只要求求出唯一的一个解。因此可以在最初的时候遍历整个数组, 将数组中的每个数字的索引放在map中。
+// 此时, record中记录的永远是每一个数字最后出现的位置。
+// 而对于 target = 2*a的情况, 如果nums中有两个或两个以上a,
+// 我们在扫描时会先看到第一个a, 而从record中拿到的是最后一个a :)
+//
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int,int> record;
+        for(int i = 0 ; i < nums.size() ; i ++)
+            record[nums[i]] = i;
+
+        for(int i = 0 ; i < nums.size() ; i ++){
+
+            if(record.find(target - nums[i]) != record.end())
+                if(record[target - nums[i]] != i)
+                    return {i, record[target - nums[i]]};
+
+            record[nums[i]] = i;
+        }
+
+        throw invalid_argument("the input has no solution");
+    }
+};
