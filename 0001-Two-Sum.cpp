34a11f294102a79b76f79f76aa545e030097f011
zuokun23 <1291136919@qq.com>
2018-06-24 15:44:48 +0800

    Create 0001-Two-Sum.cpp

---
+++ 0001-Two-Sum.cpp
@@ -0,0 +1,23 @@
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/description/
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int,int> record;
+        for(int i = 0 ; i < nums.size() ; i ++){
+
+            int complement = target - nums[i];
+            if(record.find(complement) != record.end()){
+                int res[] = {i, record[complement]};
+                return vector<int>(res, res + 2);
+            }
+
+            record[nums[i]] = i;
+        }
+
+        throw invalid_argument("the input has no solution");
+    }
+};
