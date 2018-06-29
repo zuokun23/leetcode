47d3f49f29937b64ea7f5babeff12338a610b30a
zuokun23 <1291136919@qq.com>
2018-06-29 09:51:29 +0800

    Update 0003-Longest-Substring-Without-Repeating-Character.cpp

--- 0003-Longest-Substring-Without-Repeating-Character.cpp
+++ 0003-Longest-Substring-Without-Repeating-Character.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int freq[256] = {0};
-        int l = 0 , r = -1;
+        int l = 0 , r = -1;//[l..r]为滑动窗口，窗口内不含有重复字符的子串，r=-1可以避免赋初值；也可以l=0,r=0,[l,r)表示窗口
         int res = 0;
         
         while(l < s.size()){
@@ -15,6 +15,29 @@ public:
             else
                 freq[s[l++]]--;
             
+            res = max(res , r - l + 1);
+        }
+        
+        return res;
+    }
+};
+//2
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        if(s.size() == 0)
+            return 0;
+        int l = 0 , r = 0;//[l..r]滑动窗口
+        int res = 1;//初始窗口大小
+        unordered_set<char> hash;
+        hash.insert(s[0]);
+        while(l < s.size()){
+            
+            if(r+1 < s.size() && hash.find(s[r+1]) == hash.end())
+                hash.insert(s[++r]);
+            else
+                hash.erase(s[l++]);
             res = max(res , r - l + 1);
         }
         
