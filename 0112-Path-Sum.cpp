182b0edd7e9713b6612600cc154b107d515cc7f3
zuokun23 <1291136919@qq.com>
2018-08-29 17:03:30 +0800

    Create 0112-Path-Sum.cpp

---
+++ 0112-Path-Sum.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {//root为根树是否存在和为sum的路径
+        
+        if(root == NULL)
+            return false;
+        
+        if(root->left == NULL && root->right == NULL)//叶子结点
+            return root->val == sum;
+        
+        return (hasPathSum(root->left , sum - root->val))
+        || (hasPathSum(root->right , sum - root->val));
+        
+    }
+};
